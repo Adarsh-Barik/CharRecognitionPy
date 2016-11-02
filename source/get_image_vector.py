8b943ac72cd2d8599a4591d55635b753f58b457a
adarsh <adarshbarik1@gmail.com>
2016-11-01 20:47:40 -0400

    added train, validate, test

--- source/get_image_vector.py
+++ source/get_image_vector.py
@@ -10,8 +10,8 @@ def get_image_vector(kmeans_model, image_descriptor_array):
 	" return image vectors using image descriptors and kmeans model"
 	length_of_descriptor_array = len(image_descriptor_array)
 	labels = kmeans_model.predict(image_descriptor_array)
-	image_vector = np.zeros((1, 310))
+	image_vector = np.zeros(310)
 	for i in range(length_of_descriptor_array):
 		index = int(labels[i])
-		image_vector[0][index] = image_vector[0][index] + 1
+		image_vector[index] = image_vector[index] + 1
 	return image_vector
