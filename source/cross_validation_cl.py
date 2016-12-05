4a72d63ebc0faebce0fe16cc42c8d5707e3893ba
mohkar123 <mohithrules75@gmail.com>
2016-12-04 22:12:11 -0500

    cleaned up merge errors

--- source/cross_validation_cl.py
+++ source/cross_validation_cl.py
@@ -28,7 +28,6 @@ class CrossValidation():
 		self.best_params_ = self.cv_results_['params'][best_index]
 
 
-<<<<<<< HEAD
 def store_cv_object(cvobj, out_pickle_file, out_csv_file):
 	print (" Storing cross validation object..")
 	pickle.dump(cvobj, open(out_pickle_file, 'wb'))
@@ -37,15 +36,7 @@ def store_cv_object(cvobj, out_pickle_file, out_csv_file):
 	print ("Done.")
 
 	
-||||||| merged common ancestors
-=======
-def store_cv_object(cvobj, out_pickle_file, out_csv_file):
-	pickle.dump(cvobj, open(out_pickle_file, 'wb'))
-	df = pandas.DataFrame(cvobj.cv_results_)
-	df.to_csv(out_csv_file)
 
-	
->>>>>>> 3bd080c5e0bafabf70b3c3f883100e2ecd59ed78
 def start_cross_validation(image_vectors, image_labels, out_dir, cv_type='kfold', cv_n=3, param_lin=None, param_poly=None, param_rbf=None, svm_type=None):
 	print ("Starting cross validation...")
 	svr = SVC(decision_function_shape=svm_type)
@@ -54,7 +45,6 @@ def start_cross_validation(image_vectors, image_labels, out_dir, cv_type='kfold'
 		print ("cross validation for linear kernel")
 		clf_lin = CrossValidation(svr, param_lin, cv_type=cv_type, cv_n=cv_n)
 		clf_lin.fit(image_vectors, image_labels)
-<<<<<<< HEAD
 		if svm_type == None:
 			svt = ""
 		else:
@@ -62,21 +52,12 @@ def start_cross_validation(image_vectors, image_labels, out_dir, cv_type='kfold'
 		out_p = out_dir + "clf_lin_" + cv_type + str(cv_n) + svt + ".p"  
 		out_csv = out_dir + "clf_lin_" + cv_type + str(cv_n) + svt + ".csv"  
 		store_cv_object(clf_lin, out_p, out_csv)
-||||||| merged common ancestors
-=======
-		if svm_type == None:
-			svt = ""
-		out_p = out_dir + "clf_lin_" + cv_type + str(cv_n) + svt + ".p"  
-		out_csv = out_dir + "clf_lin_" + cv_type + str(cv_n) + svt + ".csv"  
-		store_cv_object(clf_lin, out_p, out_csv)
->>>>>>> 3bd080c5e0bafabf70b3c3f883100e2ecd59ed78
 		plot_all_graphs(clf_lin, out_dir)
 
 	if param_poly:
 		print ("cross validation for poly kernel")
 		clf_poly = CrossValidation(svr, param_poly, cv_type=cv_type, cv_n=cv_n)
 		clf_poly.fit(image_vectors, image_labels)
-<<<<<<< HEAD
 		if svm_type == None:
 			svt = ""
 		
@@ -86,21 +67,12 @@ def start_cross_validation(image_vectors, image_labels, out_dir, cv_type='kfold'
 		out_p = out_dir + "clf_poly_" + cv_type + str(cv_n) + svt + ".p"  
 		out_csv = out_dir + "clf_poly_" + cv_type + str(cv_n) + svt + ".csv"  
 		store_cv_object(clf_poly, out_p, out_csv)
-||||||| merged common ancestors
-=======
-		if svm_type == None:
-			svt = ""
-		out_p = out_dir + "clf_poly_" + cv_type + str(cv_n) + svt + ".p"  
-		out_csv = out_dir + "clf_poly_" + cv_type + str(cv_n) + svt + ".csv"  
-		store_cv_object(clf_poly, out_p, out_csv)
->>>>>>> 3bd080c5e0bafabf70b3c3f883100e2ecd59ed78
 		plot_all_graphs(clf_poly, out_dir)
 
 	if param_rbf:
 		print ("cross validation for rbf kernel")
 		clf_rbf = CrossValidation(svr, param_rbf, cv_type=cv_type, cv_n=cv_n)
-		clf_lin.fit(image_vectors, image_labels)
-<<<<<<< HEAD
+		clf_rbf.fit(image_vectors, image_labels)
 		if svm_type == None:
 			svt = ""
 		else:
@@ -108,23 +80,6 @@ def start_cross_validation(image_vectors, image_labels, out_dir, cv_type='kfold'
 		out_p = out_dir + "clf_rbf_" + cv_type + str(cv_n) + svt + ".p"  
 		out_csv = out_dir + "clf_rbf_" + cv_type + str(cv_n) + svt + ".csv"  
 		store_cv_object(clf_rbf, out_p, out_csv)
-||||||| merged common ancestors
-=======
-		if svm_type == None:
-			svt = ""
-		out_p = out_dir + "clf_rbf_" + cv_type + str(cv_n) + svt + ".p"  
-		out_csv = out_dir + "clf_rbf_" + cv_type + str(cv_n) + svt + ".csv"  
-		store_cv_object(clf_rbf, out_p, out_csv)
->>>>>>> 3bd080c5e0bafabf70b3c3f883100e2ecd59ed78
 		plot_all_graphs(clf_rbf, out_dir)
-<<<<<<< HEAD
 	print ("Done.")
 
-
-
-||||||| merged common ancestors
-=======
-
-
-
->>>>>>> 3bd080c5e0bafabf70b3c3f883100e2ecd59ed78
