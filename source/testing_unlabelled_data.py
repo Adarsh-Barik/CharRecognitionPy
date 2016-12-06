17e2f981bff65f1d9e1859d308463f6b6ff0c034
mohkar123 <mohithrules75@gmail.com>
2016-12-05 22:10:13 -0500

    PUlling testLabels.csv

--- source/testing_unlabelled_data.py
+++ source/testing_unlabelled_data.py
@@ -1,7 +1,12 @@
-import ConfigParser
+from sys import version_info
+if version_info < (3, 0):
+	import ConfigParser
+else:
+	import configparser as ConfigParser
+ 
 from preprocessing import check_config_file, bmp_to_key, generate_descriptors, get_image_vectors, preprocess
 import numpy as np
-from sklearn.cluster import Kmeans
+from sklearn.cluster import KMeans
 from sklearn.svm import SVC
 
 
@@ -24,25 +29,41 @@ for file in test_files:
 	imagename = '../data/testResized/' + file
 	outkey = test_dir + 'keys/' + imagename + '.key'
 	bmp_to_key(imagename, outkey)
-
+print ("Generating test descriptors...")
 alltestdescriptors, alltestimagenames, alltestnumdescriptors = generate_descriptors(test_dir + 'keys/', False)
+print ("Done.")
+print ("Generating train descriptors...")
 train_key_dir = config.get('DEFAULT_CONFIG', 'KEY_DIR')
 alldescriptors, allimagenames, allnumdescriptors = generate_descriptors(train_key_dir, True)
+print ("Done.")
 
+print ("Generating bag of words...")
 num_cluster_centers = int(config.get('RUNTIME_CONFIG', 'NUM_OF_WORDS'))
-kmeans = Kmeans(num_cluster_centers, alldescriptors)
+kmeans = KMeans(num_cluster_centers, alldescriptors)
+print ("Done.")
 
+print ("Matching bag of words with test descriptors...")
 labels = kmeans.predict(alltestdescriptors)
+print ("Done.")
 
+print ("Generating test image vectors...")
 num_test_samples = int(config.get('TEST_CONFIG', 'NUM_SAMPLES'))
 test_image_vectors = get_image_vectors(num_test_samples, alltestnumdescriptors, labels, False)
+print ("Done.")
 
+print ("Loading train image vectors, labels...")
 train_image_vectors, train_image_labels, image_names = preprocess()
-
+print ("Done.")
 # bootstrap optimum model C=20, gamma=0.1, kernel=rbf
+print ("Training SVM...")
 clf = SVC(C=20, gamma=0.1)
 clf.fit(train_image_vectors, train_image_labels)
+print ("Done.")
+
+print ("Predicting...")
 predicted_labels = clf.predict(test_image_vectors)
+print ("Done.")
+
 
 
 
